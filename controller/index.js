bb50e37549b84c7aab72e07da1582bb3a85235ef
HieuHipHop <hieuhiphop797@mail.com>
2022-07-23 18:17:28 +0700

    finish_index

---
+++ controller/index.js
@@ -0,0 +1,37 @@
+const getAllProduct = () => {
+  var promise = axios({
+    url: "https://shop.cyberlearn.vn/api/Product",
+    method: "GET",
+  });
+  promise.then((result) => {
+    renderTable(result.data.content, "product-list");
+  });
+};
+
+const renderTable = (arr, id) => {
+  let html = "";
+  for (let i = 0; i < arr.length; i++) {
+    html += `
+    <figure class="cart-card">
+            <img
+              src="${arr[i].image}"
+              alt=""
+            />
+            <figcaption>
+              <h3>${arr[i].name}</h3>
+              <p>
+                ${arr[i].shortDescription}
+              </p>
+              <div class="price">
+                <span>$${arr[i].price}</span>
+              </div>
+            </figcaption>
+            <i class="fa-solid fa-cart-shopping"><a href="./details.html"></a></i>
+          </figure>
+    `;
+  }
+  document.getElementById(id).innerHTML = html;
+};
+window.onload = () => {
+  getAllProduct();
+};
